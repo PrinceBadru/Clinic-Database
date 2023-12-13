8b8e2fb6de60a68ecc1b7cb7b0d2a0281ee1b831
Badru Ndawula <badrundawula24@gmail.com>
2023-12-13 21:55:50 +0300

    add medical history table & key constraint

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,17 @@ CREATE TABLE patients (
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE
 );
+
+-- Create the medical_histories table
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(255)
+);
+
+-- Add a foreign key constraint to the medical_histories table
+ALTER TABLE medical_histories
+ADD CONSTRAINT fk_patient
+FOREIGN KEY (patient_id)
+REFERENCES patients(id);
